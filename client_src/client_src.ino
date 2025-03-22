a3d57e3dd7b57b958a8a965b5c16a045592d19ea
Mark Kovacs <mark69163@gmail.com>
2025-03-22 17:10:14 +0100

    Ota works, wifi manager works

--- client_src/client_src.ino
+++ client_src/client_src.ino
@@ -11,17 +11,43 @@ MFRC522 rfid(RFID_SS_PIN, RST_PIN);
 
 void setup(){
   //Serial.begin(115200);
+
+  //wifi manager setup
+  WiFiManager wm;
+  //wm.resetSettings(); //forgets previously connected network
+  if (!wm.autoConnect("dormcheckAP", "password")) {
+        //Serial.println("Failed to start as AP.");
+    }
+
+  //OTA upadte setup
+  ArduinoOTA.setHostname("dormCheck_node_1");
+  ArduinoOTA.setPassword("admin");
+  ArduinoOTA.begin();
+
+  // pheripheral setup
   init_buzzer();
   init_led();
   init_relay();
   init_door_sensor();
   init_nfc_reader();
 
-  mute_buzzer(true);
-  disable_led(true);
-  disable_relay(true);
+  //mute_buzzer(true);
+  //disable_led(true);
+  //disable_relay(true);
 }
 
 void loop(){
+  ArduinoOTA.handle();
+
+  if(is_card_present()){
+    toggle_relay(ACCEPT);
+    display_led(ACCEPT);
+    play_tune(ACCEPT);
+  }
+  else{
+    toggle_relay(DECLINE);
+    pixels.clear();
+    pixels.show();
+  }
 
 }
