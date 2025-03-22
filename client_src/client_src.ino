10f37ac6e1ce355f05915209a1a39944b995f714
Mark Kovacs <mark69163@gmail.com>
2025-03-22 16:52:41 +0100

    Added nfc reader, it works very well.

--- client_src/client_src.ino
+++ client_src/client_src.ino
@@ -4,26 +4,24 @@
 #include "led.h"
 #include "relay.h"
 #include "door_sensor.h"
-
+#include "nfc_reader.h"
 
 Adafruit_NeoPixel pixels(NUMPIXELS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
-
+MFRC522 rfid(RFID_SS_PIN, RST_PIN);
 
 void setup(){
+  //Serial.begin(115200);
   init_buzzer();
   init_led();
   init_relay();
   init_door_sensor();
+  init_nfc_reader();
 
   mute_buzzer(true);
   disable_led(true);
-  //disable_relay(true);
+  disable_relay(true);
 }
 
-void loop(){  
-  if(read_door_sensor()){
-    toggle_relay(TEST);
-  }
+void loop(){
 
-  delay(10);
 }
